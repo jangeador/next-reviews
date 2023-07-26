9bf15a4e24ceead9cba1c52da03ebe8916c764f4
Delio Castillo <delio@Delios-iMac.local>
2023-07-25 19:24:31 -0700

    Second Section

---
+++ app/reviews/hollow-knight/page.tsx
@@ -0,0 +1,10 @@
+export default function HollowKnightPage() {
+    return (
+        <>
+            <h1>Hollow Knight</h1>
+            <p>
+                Here we'll list all the review for Hollow Knight
+            </p>
+        </>
+    );
+}
