46139c919044415290972a5a76d27c8e59419016
Delio Castillo <jangeador@gmail.com>
2023-07-27 14:46:32 -0700

    adds client button

--- components/share-link-button.tsx
+++ components/share-link-button.tsx
@@ -1,3 +1,14 @@
+"use client";
 export default function ShareLinkButton() {
-  return <button className="px-2 py-1 text-sm border rounded text-slate-500 hover:bg-orange-100 hover:text-orange-700">Share Link</button>;
+  const handleClick = () => {
+    console.log("click");
+  };
+  return (
+    <button
+      onClick={handleClick}
+      className="px-2 py-1 text-sm border rounded text-slate-500 hover:bg-orange-100 hover:text-orange-700"
+    >
+      Share Link
+    </button>
+  );
 }
